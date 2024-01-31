537e844315e0ff41368d09165baa7d96879b9808
tman6290 <tofaratifarinu@gmail.com>
2024-01-31 23:40:48 +0000

    Made the user interface cleaner. and users can now sign in and out.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_31_154629) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_31_154515) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,15 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_154629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
-  end
-
-  create_table "tasks", force: :cascade do |t|
-    t.bigint "project_id", null: false
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +36,4 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_31_154629) do
   end
 
   add_foreign_key "projects", "users"
-  add_foreign_key "tasks", "projects"
 end
