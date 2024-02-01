92289e9b456758db6b5b87690df4ca834206749d
tman6290 <tofaratifarinu@gmail.com>
2024-02-01 01:11:43 +0000

    practically done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,18 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('click', function(event) {
+  const element = event.target.closest('[data-confirm]');
+  if (!element || !element.hasAttribute('data-method')) return;
+
+  const confirmMessage = element.getAttribute('data-confirm');
+  if (confirmMessage && !confirm(confirmMessage)) {
+    event.preventDefault();
+    return false;
+  }
+});
+
+
+
+
